d3afeab5b88cb0d1fc104d147ffc397101c0bfa2
Daria <alex.fev111@gmail.com>
2025-10-03 19:43:15 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+note = {}
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,6 +13,18 @@ def hello():
 @app.route("/hello/<name>")
 def greetings(name):
     return f"Привет, {name}!"
+
+'''@app.route("/submit", methods=["post"])
+def submit():
+    name = request.form["name"]
+    return f"Привет, {name}!"'''
+
+@app.route("/notes", methods=["post", "get"])
+def notes():
+    global note
+    if request.method == 'POST':
+        note[request.form["h"]] = request.form["name"]
+    return render_template("notes.html", note=note)
 
 '''@app.errorhandler(404)
 def not_found(e):
