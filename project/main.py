de30f058925a6f3f6cc51540bc4c5120f3c83da9
Daria <alex.fev111@gmail.com>
2025-10-27 15:34:07 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,33 +1,38 @@
 from flask import Flask, render_template, request
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
-note = {}
-@app.route("/")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app , db, render_as_batch=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    uni = db.Column(db.String(64), nullable=False)
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), unique=True, nullable=False)
+    subtitle = db.Column(db.String(64), unique=True, nullable=False)
+    text = db.Column(db.String(128), nullable=False)
+
+@app.route('/')
 def index():
+    '''user = User(name="Ivan", email="ivan@ivanovich.com")
+    db.session.add(user)
+    db.session.commit()'''
     return render_template("index.html")
-
-@app.route("/hello")
-def hello():
-    return 'Hello!'
-
-@app.route("/hello/<name>")
-def greetings(name):
-    return f"Привет, {name}!"
-
-'''@app.route("/submit", methods=["post"])
-def submit():
-    name = request.form["name"]
-    return f"Привет, {name}!"'''
 
 @app.route("/notes", methods=["post", "get"])
 def notes():
-    global note
     if request.method == 'POST':
-        note[request.form["h"]] = request.form["name"]
-    return render_template("notes.html", note=note)
+        note = Notes(title=request.form["h"], subtitle=request.form["s"], text=request.form["name"])
+        db.session.add(note)
+        db.session.commit()
+    n = Notes.query.all()
+    return render_template("notes.html", n=n)
 
-'''@app.errorhandler(404)
-def not_found(e):
-    return render_template("404.html"), 404'''
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
